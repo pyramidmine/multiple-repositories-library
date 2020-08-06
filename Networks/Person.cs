3820a81e7e004300b5665d6f3a71a3f630213fc4
pyramidmine <pyramidmine@gmail.com>
2020-08-06 12:38:50 +0900

    최초 커밋

---
+++ Networks/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MannaPlanet.Networks
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string GetInfo()
+        {
+            Debug.WriteLineIf(Age <= 0, $"{Name}'s age is 0.");
+            return string.Format($"{Name} is {Age} years old.");
+        }
+    }
+}
